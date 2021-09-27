aebd95041dbe6c10c825a28ae869152cb9d0a5e9
Ignacio Gonzalez <nchngz@gmail.com>
2021-09-27 20:04:48 -0300

    refactor of core module to use sparse matrix representation for graphs.

--- src/numlenology/core.py
+++ src/numlenology/core.py
@@ -1,7 +1,8 @@
-import pickle
 from collections import defaultdict
 
 import graphviz as gv
+import numpy as np
+# import scipy
 from num2words import CONVERTER_CLASSES, num2words
 
 from numlenology.utils import float_range, rank_keys
@@ -9,13 +10,48 @@ from numlenology.utils.color import fade_color
 
 
 LANGS = sorted(set(lang[:2] for lang in CONVERTER_CLASSES.keys()))
-PICKLE_FILENAME = "./data/{lang}-{bound}.pickle"
 
 NODE_COLOR_LEAF = "#009000bb"
 NODE_COLOR_ROOT = "#000000bb"
 NODE_SIZE_MIN = 0.4
 NODE_SIZE_MAX = 0.9
 
+
+def recursive_build_chain(lang, matrix, i, visited):
+    visited.append(i)
+    j = len(num2words(i, lang=lang))
+    if j in visited:
+        # add the last edge to "close" the loop.
+        matrix[i][j] += 1
+        return
+    else:
+        matrix[i][j] += 1
+        return recursive_build_chain(lang, matrix, j, visited)
+
+
+def build_graph_matrix_rec(bound=10, lang="es"):
+    bound += 1  # from 0 to `bund` included.
+    matrix = np.zeros([bound, bound])
+    for i in range(bound):
+        recursive_build_chain(lang, matrix, i, [])
+    return matrix
+
+
+def build_graph_matrix(bound=10, lang="es"):
+    bound += 1  # from 0 to `bund` included.
+    matrix = np.zeros([bound, bound])
+    for i in range(bound):
+        visited = [i]
+        while (j := len(num2words(i, lang=lang))) not in visited:
+            matrix[i][j] += 1
+            visited.append(j)
+            i = j
+        # add the last edge to "close" the loop.
+        matrix[i][j] += 1
+    return matrix
+
+
+# old methods.
 
 def build_single_chain(n, lang="en"):
     """
@@ -54,33 +90,16 @@ def build_single_chain(n, lang="en"):
     return visited, end_loop
 
 
-def build_graph(bound=100, lang="en", dump_pickle=False):
+def build_graph(bound=100, lang="en"):
     """
     Call `build_single_chain` for each number from 0 to `bound` included
-    and return a graphviz.Digraph object.
-
-    If `dump_pickle` then a file is generated containing a dict with the
-    following keys:
+    and return the following tuple:
         - connected_comp: dict {end_loop: nodes}. `nodes` is the set of nodes
             in each connected component, characterized by `end_loop`.
         - edges: each tuple (A, B) in this set is an edge A->B.
         - grade_by_node: dict {node: grade}. only NOT leaf nodes are here.
         - depth_by_node: dict {node: depth}. all nodes are here.
     """
-    g = gv.Digraph(
-        engine="neato",
-        node_attr={
-            "style": "filled",
-            "color": NODE_COLOR_LEAF,
-            "fixedsize": "true",
-            "width": str(NODE_SIZE_MIN),
-            "height": str(NODE_SIZE_MIN),
-        },
-        graph_attr={
-            "outputorder": "nodesfirst",
-        },
-    )
-
     connected_comp = defaultdict(set)
     graph_edges = set()
     grade_by_node = defaultdict(int)
@@ -90,8 +109,8 @@ def build_graph(bound=100, lang="en", dump_pickle=False):
         visited, end_loop = build_single_chain(n, lang)
         depth_by_node[n] = len(visited) - len(end_loop)
 
-        # use a set of tuples to find unique end loops.
-        connected_comp[sorted(end_loop)].add(n)
+        # # use a set of tuples to find unique end loops.
+        # connected_comp[tuple(sorted(end_loop))].add(n)
 
         # count node grade. we skip the first visited node, it's the leaf
         # for this single chain.
@@ -105,6 +124,13 @@ def build_graph(bound=100, lang="en", dump_pickle=False):
         # add the last edge that completes the loop.
         edges += [(end_loop[-1], end_loop[0])]
         graph_edges |= set(edges)
+
+    return connected_comp, graph_edges, grade_by_node, depth_by_node
+
+
+def build_graphviz(bound, lang):
+    """Call `build_graph` and return a graphviz.Digraph object."""
+    _, graph_edges, grade_by_node, depth_by_node = build_graph(bound, lang)
 
     depths = rank_keys(depth_by_node, reverse=True)
     color_scale = fade_color(
@@ -120,7 +146,21 @@ def build_graph(bound=100, lang="en", dump_pickle=False):
         1 + max(grades.values()),
     )
 
-    # build graph. cast objects to str, graphviz requires this type.
+    # build graph. graphviz requires objects to be cast to str.
+    g = gv.Digraph(
+        engine="neato",
+        node_attr={
+            "style": "filled",
+            "color": NODE_COLOR_LEAF,
+            "fixedsize": "true",
+            "width": str(NODE_SIZE_MIN),
+            "height": str(NODE_SIZE_MIN),
+        },
+        graph_attr={
+            "outputorder": "nodesfirst",
+        },
+    )
+
     for node, grade in grades.items():
         g.node(
             str(node),
@@ -131,15 +171,5 @@ def build_graph(bound=100, lang="en", dump_pickle=False):
 
     for e in graph_edges:
         g.edge(*map(str, e))
-
-    if dump_pickle:
-        graph_data = {
-            "connected_comp": connected_comp,
-            "edges": graph_edges,
-            "grade_by_node": grade_by_node,
-            "depth_by_node": depth_by_node,
-        }
-        with open(PICKLE_FILENAME.format(lang=lang, bound=bound), "wb") as f:
-            pickle.dump(graph_data, f)
 
     return g
